703fd7c1e074773c9284de05c0c056d575756dee
Bogdan Vesylivskyy <vesylivskyy@gmail.com>
2018-08-09 14:42:50 +0300

    new version

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    $('a[href^="#"]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - -10
+        }, 800);
+        e.preventDefault();
+    });
+    return false;
+});
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+    $('#home').css({
+        'height': $(window).height()
+    });
+
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        center: true,
+        loop: true,
+        nav: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 1520,
+        smartSpeed: 1500,
+        animateIn: 'linear',
+        animateOut: 'linear',
+        margin: 10,
+        stagePadding: 20, 
+        responsive:{ 
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        970:{
+            items:3
+        }
+        }
+    })
+
+
+    $(function() {
+        var header = $("header");
+        
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+            if (scroll >= 46) {
+                header.addClass("scrolled");
+            } else {
+                header.removeClass("scrolled");
+            }
+        });
+        
+    });
+    
+
+});
